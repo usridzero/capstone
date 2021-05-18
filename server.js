5348eda5ee87a812d8463a41ac241956c897b221
usridzero <usridzero@gmail.com>
2021-05-18 16:53:52 -0400

    reverting changes to server.js.

--- server.js
+++ server.js
@@ -6,12 +6,8 @@ const HOST = "0.0.0.0";
 
 // App
 const app = express();
-//app.get("/", (req, res) => res.send(
-//"This is my Caltech PGP Capstone Project / Jose Pagan, 703-957-5737 -- Hello World, Omar was here!!!"));
-app.get("/", (req, res) => {
-  res.send("This is my Caltech PGP Capstone Project"
-  "Jose Pagan, 703-957-5737"
-  "Hello World, Jose was here!!!"));
+app.get("/", (req, res) => res.send(
+"This is my Caltech PGP Capstone Project / Jose Pagan, 703-957-5737 -- Hello World, Omar was here!!!"));
 
 let server = app.listen(PORT, () => {
   console.log(`Running on http://${HOST}:${PORT}`);
