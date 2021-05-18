cf5f2b5c6d681933a6c0b02b2d66cc3e899fd987
usridzero <usridzero@gmail.com>
2021-05-18 16:02:42 -0400

    fixing webapp.

--- server.js
+++ server.js
@@ -7,9 +7,7 @@ const HOST = "0.0.0.0";
 // App
 const app = express();
 app.get("/", (req, res) => res.send(
-"This is my Caltech PGP Capstone Project"
-"Jose Pagan, 703-957-5737"
-"Hello World, Omar was here!!!"));
+"This is my Caltech PGP Capstone Project / Jose Pagan, 703-957-5737 -- Hello World, Omar was here!!!"));
 
 let server = app.listen(PORT, () => {
   console.log(`Running on http://${HOST}:${PORT}`);
