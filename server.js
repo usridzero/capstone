cccd5c10dccd4e4d9d1d4697540d81685e32b083
usridzero <usridzero@gmail.com>
2021-05-18 16:31:13 -0400

    testing new server.js config.

--- server.js
+++ server.js
@@ -6,8 +6,12 @@ const HOST = "0.0.0.0";
 
 // App
 const app = express();
-app.get("/", (req, res) => res.send(
-"This is my Caltech PGP Capstone Project / Jose Pagan, 703-957-5737 -- Hello World, Omar was here!!!"));
+//app.get("/", (req, res) => res.send(
+//"This is my Caltech PGP Capstone Project / Jose Pagan, 703-957-5737 -- Hello World, Omar was here!!!"));
+app.get("/", (req, res) => {
+  res.send("This is my Caltech PGP Capstone Project")
+  res.send("Jose Pagan, 703-957-5737")
+	res.send("Hello World, Jose was here!!!"));
 
 let server = app.listen(PORT, () => {
   console.log(`Running on http://${HOST}:${PORT}`);
