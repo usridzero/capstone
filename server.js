1085c573fecbc62c57e32c2c556519c8f5a14b8d
usridzero <usridzero@gmail.com>
2021-05-18 16:41:58 -0400

    testing new server.js file.

--- server.js
+++ server.js
@@ -9,9 +9,9 @@ const app = express();
 //app.get("/", (req, res) => res.send(
 //"This is my Caltech PGP Capstone Project / Jose Pagan, 703-957-5737 -- Hello World, Omar was here!!!"));
 app.get("/", (req, res) => {
-  res.send("This is my Caltech PGP Capstone Project")
-  res.send("Jose Pagan, 703-957-5737")
-	res.send("Hello World, Jose was here!!!"));
+  res.send("This is my Caltech PGP Capstone Project"
+  "Jose Pagan, 703-957-5737"
+  "Hello World, Jose was here!!!"));
 
 let server = app.listen(PORT, () => {
   console.log(`Running on http://${HOST}:${PORT}`);
